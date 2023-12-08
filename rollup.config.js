562a31faced7dbb9d700083eee28e95995f4b26e
apple <>
2023-12-08 14:44:57 +0800

    feat: 新增支持自动化发布

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-const typescript = require('@rollup/plugin-typescript')
+const typescript = require("rollup-plugin-typescript2");
 const sourceMaps = require('rollup-plugin-sourcemaps')
 const commonjs = require('@rollup/plugin-commonjs')
 const resolve = require('@rollup/plugin-node-resolve')
@@ -10,46 +10,61 @@ module.exports = {
   input: './src/index.ts',
   output: [
     {
+      format: "iife",
+      dir: path.dirname(`dist/browser/index.js`),
+      preserveModules: true,
+      name: "tsDemo", // 包名 自动挂载到window
+      preserveModulesRoot: "src",
+    },
+    {
+      format: "iife",
+      name: "tsDemo",
+      file: "dist/browser/index.min.js",
+      plugins: [terser()],
+    },
+    {
       format: 'cjs',
-      dir: path.dirname(`lib/index.cjs.js`),
+      dir: path.dirname(`dist/lib/index.cjs.js`),
       preserveModules: true,
       preserveModulesRoot: 'src',
     },
     {
       format: 'cjs',
-      file: 'lib/index.cjs.min.js',
+      file: 'dist/lib/index.cjs.min.js',
       plugins:[terser()]
     },
     {
       format: 'es',
-      dir: path.dirname('es/index.esm.js'),
+      dir: path.dirname('dist/es/index.esm.js'),
       preserveModules: true,
       preserveModulesRoot: 'src',
     },
     {
       format: 'es',
-      file: 'es/index.esm.min.js',
+      file: 'dist/es/index.esm.min.js',
       plugins:[terser()]
     },
     {
       format: 'umd',
       name: 'bundle.umd',
-      file: 'umd/index.umd.js',
+      file: 'dist/umd/index.umd.js',
       inlineDynamicImports: true,
     },
     {
       format: 'umd',
       name: 'bundle.umd',
-      file: `umd/index.umd.min.js`,
+      file: `dist/umd/index.umd.min.js`,
       inlineDynamicImports: true,
       plugins:[terser()]
     },
   ],
   plugins: [
     autoExternal(),
+    typescript({
+      useTsconfigDeclarationDir: true, // 使用tsconfig的ts输出路径
+    }),
     commonjs({ extensions: ['.js', '.ts'] }),
     resolve(),
-    typescript(),
     sourceMaps(),
   ],
 }
